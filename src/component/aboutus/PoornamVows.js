11d0ef17fd765409f659fc2474a5a19ec9dcb90f
rajul-09 <rajuldhimole9@gmail.com>
2025-07-08 10:26:03 +0530

    vows rotation

--- src/component/aboutus/PoornamVows.js
+++ src/component/aboutus/PoornamVows.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import { Box, Typography, Grid, Avatar } from '@mui/material';
-
+import { keyframes } from '@emotion/react';
 
 
 const vows = [
-  { img: '/AboutVows3.png', title: 'Emotions Ka Khayal' },
-  { img: '/AboutVows4.png', title: 'Riwaaz Ko Rang Denge' },
-  { img: '/AboutVows5.png', title: 'Aapki Tension, Ab Hamari' },
-  { img: '/AboutVows6.png', title: 'Banayenge Sapne Ko Haqiqat' },
-  { img: '/AboutVows7.png', title: 'Sunenge Dil Se' },
-  { img: '/AboutVows1.png', title: 'Yaadon Ka Wada' },
-  { img: '/AboutVows2.png', title: 'Har Scene, Screen-Ready' },
+  { img: '/AboutVows3.png' },
+  { img: '/AboutVows4.png' },
+  { img: '/AboutVows5.png' },
+  { img: '/AboutVows6.png' },
+  { img: '/AboutVows7.png' },
+  { img: '/AboutVows1.png' },
+  { img: '/AboutVows2.png' },
 ];
 
 
-
+const rotate = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
 
 const PoornamVows = () => {
   return (
@@ -34,24 +41,19 @@ const PoornamVows = () => {
           mx: 'auto',
         }}
       >
-     
-        {/* Vows arranged in a circle */}
-          {/* Rotating container of vow items */}
 
- <Box
+        {/* Vows arranged in a circle */}
+        {/* Orbiting container */}
+        <Box
           sx={{
             position: 'absolute',
             width: '100%',
             height: '100%',
-            animation: 'orbit 20s linear infinite',
-            transformOrigin: 'center center',
-            '@keyframes orbit': {
-              from: { transform: 'rotate(0deg)' },
-              to: { transform: 'rotate(360deg)' },
-            },
+            transform: 'translate(-50%, -50%)',
+            animation: `${rotate} 20s linear infinite`,
           }}
         >
-       
+
           {vows.map((vow, index) => {
             const angle = (index / vows.length) * 2 * Math.PI;
             const radius = 220;
@@ -104,8 +106,8 @@ const PoornamVows = () => {
             variant="circular"
             sx={{ width: '100%', height: '100%' }}
           />
-        
-         
+
+
         </Box>
       </Box>
     </Box>
