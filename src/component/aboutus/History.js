4e69942a09144da83270f15f82641807389fc2eb
rajul-09 <rajuldhimole9@gmail.com>
2025-07-09 13:42:16 +0530

    ourStories

--- src/component/aboutus/History.js
+++ src/component/aboutus/History.js
@@ -18,10 +18,10 @@ export default function OurStories() {
   return (
     <div style={{ padding: "4rem", fontFamily: "Akatab", fontSize: { xs: "0.8rem", sm: "0.8rem", md: "1.2rem" }}}>
       {data.map((item, index) => (
-        <div key={index} style={{ marginBottom: "0.2rem" }}>
+        <div key={index} style={{ marginBottom: "0.5rem" }}>
           <h4 style={{ color: "black" }}>{item.heading}</h4>
           {item.paragraphs.map((p, i) => (
-            <p key={i} style={{ lineHeight: 1 }}>{p}</p>
+            <p key={i} style={{ lineHeight: 1.2 }}>{p}</p>
           ))}
         </div>
       ))}
