1aa0159691d3124d72a476e2239e27ada61ed4e8
rajul-09 <rajuldhimole9@gmail.com>
2025-07-09 11:51:40 +0530

    new changes

--- src/component/aboutus/AboutUs.js
+++ src/component/aboutus/AboutUs.js
@@ -1,18 +1,30 @@
 import React from "react";
-import AboutUsBanner from "./AboutUsBanner";
 import OurStories from "./OurStories";
 import MagicMakers from "./MagicMakers";
 import Owners from "./Owners"
-import  History  from "./History";
+import History from "./History";
 import PoornamVows from "./PoornamVows";
 import Philosophy from "./Philosophy";
 import Snapshot from "./Snapshot";
 import ContactSection from "../home/ContactSection";
+import CustomBanner from "@/common-component/banner/CustomBanner";
+import CustomButton from "@/common-component/button/CustomButton";
 
 export default function AboutUS() {
     return (
         <>
-            <AboutUsBanner></AboutUsBanner>
+            <CustomBanner
+                title="Let's Make Shaadi Magic Together!"
+                subtitle="From planning to 'I do', we' ve got your back!"
+                // paragraphSubtitle="Welcome to the only wedding blog that understands your vibe, your chaos, and your cousin who always wants to dance at haldi. From serious planning tips to hilarious shaadi stories, we’ve written it all — with ❤️, dhol, and a bit of dholak. For brides, grooms, families, and even the baaratis. Let’s get planning, Poornam-style."
+                backgroundImage="/AboutUsBanner.png"
+                showLogo={true}
+                logoSrc="/logo.png"
+            >
+                <CustomButton>Plan my Wedding</CustomButton>
+            </CustomBanner >
+
+         
             <OurStories></OurStories>
             <MagicMakers></MagicMakers>
             <Owners></Owners>
--- src/component/home/AboutSection.js
+++ src/component/home/AboutSection.js
@@ -1,63 +1,75 @@
 import React from "react";
+import { Link } from '@mui/material';
 import { Box, Typography, Button, Container } from "@mui/material";
 import CustomButton from "@/common-component/button/CustomButton";
 
 export default function AboutSection() {
   return (
-    <Box sx={{ bgcolor: "#fff", marginTop: "10px", marginBottom: "10px" ,py:{xs:1, md:2},
-        px: { xs: 2, md: 7 },}}>
-     
-        <Typography
-          variant="h4"
-          sx={{
-            fontFamily: 'Gloock, serif',
-            fontWeight: "400",
-            color: "#0D1A46",
-            mb: 3,
-          }}
-        >
-         {` Crafting Weddings with Soul Since 2017`}
-        </Typography>
+    <Box sx={{
+      bgcolor: "#fff", marginTop: "10px", marginBottom: "10px", py: { xs: 1, md: 2 },
+      px: { xs: 2, md: 7 },
+    }}>
 
-        <Typography
-          variant="body1"
-          sx={{
-            color: "#000D1F",
-            fontSize: "1rem",
-            lineHeight: 1.75,
-            frontFamily: "Akatab,Sans-serif",
-            color: "#000000",
-            fontWeight:'700' ,
-            // textAlign: "center",
-            mb: 2,
-          }}
-        >
-         {` At Poonam Events, we turn your wedding dreams into unforgettable
+      <Typography
+        variant="h4"
+        sx={{
+          fontFamily: 'Gloock, serif',
+          fontWeight: "400",
+          color: "#0D1A46",
+          mb: 3,
+        }}
+      >
+        {` Crafting Weddings with Soul Since 2017`}
+      </Typography>
+
+      <Typography
+        variant="body1"
+        sx={{
+          color: "#000D1F",
+          fontSize: "1rem",
+          lineHeight: 1.75,
+          frontFamily: "Akatab,Sans-serif",
+          color: "#000000",
+          fontWeight: '700',
+          // textAlign: "center",
+          mb: 2,
+        }}
+      >
+        {` At Poonam Events, we turn your wedding dreams into unforgettable
           memories. Founded by <strong>Aadi Jain</strong>, a self-taught planner
           with a passion for perfection, and <strong>Richi Anand Jain</strong>,
           the creative spirit behind our signature style — we've been planning
           heartfelt celebrations across India since 2017.`}
-        </Typography>
+      </Typography>
 
-        <Typography
-          variant="body1"
-          sx={{
-            color: "#000000",
-            fontSize: "1rem",
-            lineHeight: 1.75,
-            frontFamily: "Akatab,Sans-serif",
-            color: "#000000",
-            fontWeight:'700' ,
-            mb: 4,
-          }}
-        >
+      <Typography
+        variant="body1"
+        sx={{
+          color: "#000000",
+          fontSize: "1rem",
+          lineHeight: 1.75,
+          frontFamily: "Akatab,Sans-serif",
+          color: "#000000",
+          fontWeight: '700',
+          mb: 4,
+        }}
+      >
         {`  From intimate gatherings to grand destination weddings, our team
           brings emotion, elegance, and flawless execution to every event.`}
-        </Typography>
+      </Typography>
 
-        <Box display="flex">
-          <CustomButton> {`Meet Our Story`}</CustomButton>
-        </Box>
+      <Box display="flex">
+        <CustomButton>
+          <Link
+            href="/aboutus"
+            color="inherit"
+            underline="none"
+
+          >
+            {`Meet Our Story`}
+          </Link>
+        </CustomButton>
+      </Box>
     </Box>
   );
 }
