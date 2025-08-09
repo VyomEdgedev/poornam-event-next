a389277fab4cf0cccbedca00860a2638b2154ec5
aniket0806 <97946316+aniket0806@users.noreply.github.com>
2025-08-09 20:04:19 +0530

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -43,6 +43,7 @@ export default function App({ Component, pageProps }) {
     <>
       <Head>
         <title>Poornam_Events</title>
+        <link rel="icon" href="/favicon.ico" />
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width"
