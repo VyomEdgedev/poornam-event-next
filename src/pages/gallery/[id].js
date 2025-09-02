6ea5745082f813282ce2516808e2e3742322c3f5
Aniket Gupta <97946316+aniket0806@users.noreply.github.com>
2025-09-02 10:58:12 +0530

    apply single api (#188)

--- src/pages/gallery/[id].js
+++ src/pages/gallery/[id].js
@@ -1,10 +1,33 @@
 import FilterGallery from '@/component/portfolio/FilterGallery'
+import { apiClient } from '@/lib/api-client'
 import React from 'react'
 
-const id = () => {
+const id = ({galleryFilter}) => {
   return (
-    <FilterGallery/>
+    <FilterGallery galleryFilter={galleryFilter}/>
   )
 }
 
 export default id
+
+export async function getServerSideProps() {
+  try {
+        const response = await apiClient.get("/api/portfolio/event");
+        const data = response.data;
+      if (!data || Object.keys(data).length === 0) {
+      return { notFound: true };
+      }
+    return {
+      props: {
+        galleryFilter: Array.isArray(data) ? data : [],
+      },
+    };
+  } catch (error) {
+    console.error("Error fetching gallery data:", error.message);
+    return {
+      props: {
+        galleryFilter: [],
+      },
+    };
+  }
+}
