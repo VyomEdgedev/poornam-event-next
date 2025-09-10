fb19e7ab39f3f72a553ec2ad2b004e1c883bc915
aniket0806 <97946316+aniket0806@users.noreply.github.com>
2025-09-11 02:57:23 +0530

    resolve bugs

--- src/pages/blog/[id].js
+++ src/pages/blog/[id].js
@@ -11,80 +11,80 @@ const id = ({ singleBlog }) => {
 
 export default id;
 
-export async function getServerSideProps({ params }) {
-  const { id } = params || {};
-
-  const baseUrl = process.env.NEXT_PUBLIC_SERVER_URL;
-  const url = `${baseUrl}/api/blogs/${id}/event`;
-
-  try {
-    const singleBlogResponse = await axios.get(url);
-    
-    if (!singleBlogResponse || Object.keys(singleBlogResponse).length === 0) {
-      return { notFound: true };
-    }
-    return {
-      props: {
-        singleBlog: singleBlogResponse.data.blog || [],
-      },
-    };
-  } catch (error) {
-   console.error("err", error.message);
-    return {
-      notFound: true,
-    };
-  }
-}
-
-
-
-// export async function getStaticPaths() {
-//   const baseUrl = process.env.NEXT_PUBLIC_SERVER_URL;
-//   const url = `${baseUrl}/api/blogs`;
-
-//   try {
-//     const response = await axios.get(url);
-//     const blogs = response.data.blogs || [];
-
-//     const paths = blogs.map((blog) => ({
-//       params: { id: blog.id.toString() }, // Make sure id is a string
-//     }));
-
-//     return {
-//       paths,
-//       fallback: "blocking", // "blocking" will server-render new paths not generated at build
-//     };
-//   } catch (error) {
-//     console.error("Error fetching blogs:", error.message);
-//     return {
-//       paths: [],
-//       fallback: "blocking",
-//     };
-//   }
-// }
-
-// // This function fetches blog data at build time
-// export async function getStaticProps({ params }) {
+// export async function getServerSideProps({ params }) {
 //   const { id } = params || {};
+
 //   const baseUrl = process.env.NEXT_PUBLIC_SERVER_URL;
 //   const url = `${baseUrl}/api/blogs/${id}/event`;
 
 //   try {
-//     const response = await axios.get(url);
-//     const singleBlog = response.data.blog || null;
-
-//     if (!singleBlog) {
+//     const singleBlogResponse = await axios.get(url);
+    
+//     if (!singleBlogResponse || Object.keys(singleBlogResponse).length === 0) {
 //       return { notFound: true };
 //     }
-
 //     return {
 //       props: {
-//         singleBlog,
+//         singleBlog: singleBlogResponse.data.blog || [],
 //       },
-//       revalidate: 60, // optional: regenerate the page every 10 seconds
 //     };
 //   } catch (error) {
-//     console.error("Error fetching single blog:", error.message);
-//     return { notFound: true };
+//    console.error("err", error.message);
+//     return {
+//       notFound: true,
+//     };
 //   }
 // }
+
+
+
+export async function getStaticPaths() {
+  const baseUrl = process.env.NEXT_PUBLIC_SERVER_URL;
+  const url = `${baseUrl}/api/blogs`;
+
+  try {
+    const response = await axios.get(url);
+    const blogs = response.data.blogs || [];
+
+    const paths = blogs.map((blog) => ({
+      params: { id: blog.id.toString() }, // Make sure id is a string
+    }));
+
+    return {
+      paths,
+      fallback: "blocking", // "blocking" will server-render new paths not generated at build
+    };
+  } catch (error) {
+    console.error("Error fetching blogs:", error.message);
+    return {
+      paths: [],
+      fallback: "blocking",
+    };
+  }
+}
+
+// This function fetches blog data at build time
+export async function getStaticProps({ params }) {
+  const { id } = params || {};
+  const baseUrl = process.env.NEXT_PUBLIC_SERVER_URL;
+  const url = `${baseUrl}/api/blogs/${id}/event`;
+
+  try {
+    const response = await axios.get(url);
+    const singleBlog = response.data.blog || null;
+
+    if (!singleBlog) {
+      return { notFound: true };
+    }
+
+    return {
+      props: {
+        singleBlog,
+      },
+      revalidate: 60, // optional: regenerate the page every 10 seconds
+    };
+  } catch (error) {
+    console.error("Error fetching single blog:", error.message);
+    return { notFound: true };
+  }
+}
