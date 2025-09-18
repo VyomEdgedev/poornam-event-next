52eec45afa070c2b3bfa1e4dc57c5f1ac7a012fc
Ajay Kushwaha <129685062+ajaykushwaha0508@users.noreply.github.com>
2025-09-18 15:51:57 +0530

    removed sub blog page (#202)
    Co-authored-by: Ajay <ajaykushwaha626162@gmail.com>

--- src/pages/servicessubpage.js
+++
@@ -1,7 +0,0 @@
-// src/pages/servicessubpage.js
-import ServicesSubPage from "@/component/ServicesSubPage/ServicesSubPage";
-
-// 👇 Change the function name so it's NOT the same as the import
-export default function ServicesSubPagePage() {
-  return <ServicesSubPage />;
-}
